92ace9d9d0c75e7e1bdbd14a3ff089dc5582d9de
boraarslan <bora.rs@pm.me>
2022-11-13 21:21:49 +0300

    Use Docker for database tests (#5)

---
+++ mi-db/build.rs
@@ -0,0 +1,22 @@
+use dotenvy::dotenv;
+use sqlx::postgres::PgPoolOptions;
+
+#[tokio::main]
+async fn main() {
+    // trigger recompilation when a new migration is added
+    println!("cargo:rerun-if-changed=migrations");
+    // Ignore error if .env is not found
+    _ = dotenv();
+    // Only run migrations in CI
+    let ci_env = std::env::var("MAPPER_INFLUENCE_CI_ENV").is_ok();
+    if ci_env {
+        let db_url = std::env::var("DATABASE_URL").unwrap();
+        let db = PgPoolOptions::new()
+            .max_connections(20)
+            .connect(&db_url)
+            .await
+            .unwrap();
+
+        sqlx::migrate!().run(&db).await.unwrap();
+    }
+}
