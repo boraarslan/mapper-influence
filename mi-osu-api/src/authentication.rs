ffe730fca9b13db7b07c847d71baa7f78a7b0570
112batuhan <batuhancakir97@hotmail.com>
2022-11-19 13:00:39 +0300

    adding refresh token body builder

--- mi-osu-api/src/authentication.rs
+++ mi-osu-api/src/authentication.rs
@@ -6,20 +6,45 @@ use thiserror::Error;
 pub struct AuthRequestBody {
     pub client_id: String,
     pub client_secret: String,
-    pub code: String,
     pub grant_type: String,
+    pub redirect_uri: String,
+    pub code: Option<String>,
+    pub refresh_token: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct AuthRequestBuilder {
+    pub client_id: String,
+    pub client_secret: String,
     pub redirect_uri: String,
 }
 
-impl AuthRequestBody {
-    #[allow(dead_code)]
-    pub fn new(client_id: String, client_secret: String, code: String) -> Self {
-        AuthRequestBody {
+impl AuthRequestBuilder {
+    pub fn new(client_id: String, client_secret: String, redirect_uri: String) -> Self {
+        Self {
             client_id,
             client_secret,
-            code,
+            redirect_uri,
+        }
+    }
+    pub fn request_body(&self, code: String) -> AuthRequestBody {
+        AuthRequestBody {
+            client_id: self.client_id.clone(),
+            client_secret: self.client_secret.clone(),
+            redirect_uri: self.redirect_uri.clone(),
             grant_type: "authorization_code".to_string(),
-            redirect_uri: "https://mapper-influences.vercel.app/oauth".to_string(),
+            code: Some(code),
+            refresh_token: None,
+        }
+    }
+    pub fn refresh_body(&self, refresh_token: String) -> AuthRequestBody {
+        AuthRequestBody {
+            client_id: self.client_id.clone(),
+            client_secret: self.client_secret.clone(),
+            redirect_uri: self.redirect_uri.clone(),
+            grant_type: "refresh_token".to_string(),
+            code: None,
+            refresh_token: Some(refresh_token),
         }
     }
 }
@@ -89,17 +114,25 @@ async fn request_token(
 mod tests {
     use super::*;
 
-    #[ignore]
     #[tokio::test]
     async fn test() {
         dotenv::dotenv().unwrap();
 
-        let body = AuthRequestBody::new(
+        let client = reqwest::Client::new();
+
+        let auth_request_builder = AuthRequestBuilder::new(
             std::env::var("CLIENT_ID").unwrap(),
             std::env::var("CLIENT_SECRET").unwrap(),
-            "code".to_string(),
+            std::env::var("REDIRECT_URI").unwrap(),
         );
-        let client = reqwest::Client::new();
-        request_token(&client, body).await.unwrap();
+
+        // get fresh code from https://mapper-influences.vercel.app/oauth
+        // It's single use.
+        let code = "code".to_string();
+        let body = auth_request_builder.request_body(code);
+        let first_response = dbg!(request_token(&client, body).await.unwrap());
+
+        let body = auth_request_builder.refresh_body(first_response.refresh_token);
+        let _ = dbg!(request_token(&client, body).await.unwrap());
     }
 }
