0c53ecb4afd719fc86d11a6900963edd0865cfb9
Fursum <fursum0@gmail.com>
2023-03-05 12:41:25 +0300

    implement user service in profile pages

--- mi-ui/src/pages/profile/index.tsx
+++ mi-ui/src/pages/profile/index.tsx
@@ -1,32 +1,40 @@
 import { NextPage } from "next";
 import ProfilePage from "@components/PageComponents/ProfilePage";
 import { User } from "@libs/types/user";
-
+import { useGetUserBase } from "src/services/userBase";
 
 const MapperPage: NextPage = () => {
-  return (
-    <>
-      <ProfilePage userData={userData} editable />
-    </>
-  );
+  const { user, error, loading } = useGetUserBase();
+
+  let mergedData = dummyData;
+  if (user)
+    mergedData = {
+      ...dummyData,
+      ...user,
+    };
+
+  if (error) return <h1>Error while fetching user: {error}</h1>;
+  if (loading) return <span>Loading...</span>;
+  if (user) return <ProfilePage userData={mergedData} editable />;
+  return <h1>User not found! {error}</h1>;
 };
 
 export default MapperPage;
 
-const userData: User = {
-    description: "",
-    details: {
-      followerCount: 1,
-      graveyardCount: 1,
-      lovedCount: 0,
-      pendingCount: 1,
-      rankedCount: 0,
-      subCount: 2,
-    },
-    id: 12345,
-    influences: [],
-    maps: [],
-    mentions: [],
-    username: "Skytuna",
-    avatarUrl: "https://a.ppy.sh/4865030?1650115534.jpeg",
-  };
+const dummyData: User = {
+  description: "",
+  details: {
+    followerCount: 1,
+    graveyardCount: 1,
+    lovedCount: 0,
+    pendingCount: 1,
+    rankedCount: 0,
+    subCount: 2,
+  },
+  id: 12345,
+  influences: [],
+  maps: [],
+  mentions: [],
+  username: "Skytuna",
+  avatarUrl: "https://a.ppy.sh/4865030?1650115534.jpeg",
+};
