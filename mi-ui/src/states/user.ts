8d52243c3e6f466b66df4e857cbe93d751cd227c
Fursum <fursum0@gmail.com>
2023-03-05 12:40:22 +0300

    change session store type

--- mi-ui/src/states/user.ts
+++ mi-ui/src/states/user.ts
@@ -1,19 +1,22 @@
 import { create } from "zustand";
 import { devtools, persist } from "zustand/middleware";
-import { User } from "@libs/types/user";
+import { User, UserBase } from "@libs/types/user";
 
 type SessionStore = {
   authKey?: string;
-  user?: User;
-  login: (user: User, key: string) => void;
+  user?: UserBase;
+  login: (user: UserBase, key: string) => void;
   logout: () => void;
 };
 
 export const useSessionStore = create<SessionStore>()(
   devtools(
-    persist((set) => ({
-      login: (user, key) => set({ user: user, authKey: key }),
-      logout: () => set({ user: undefined, authKey: undefined }),
-    }), { name: "userStore" })
+    persist(
+      (set) => ({
+        login: (user, key) => set({ user: user, authKey: key }),
+        logout: () => set({ user: undefined, authKey: undefined }),
+      }),
+      { name: "userStore" }
+    )
   )
 );
