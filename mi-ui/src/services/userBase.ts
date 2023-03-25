5990623f0787585553e0b724a5a0421175f275f5
Fursum <fursum0@gmail.com>
2023-03-25 11:57:33 +0300

    fix get user service

--- mi-ui/src/services/userBase.ts
+++ mi-ui/src/services/userBase.ts
@@ -21,8 +21,10 @@ export async function getUserBase(userId?: string) {
       1000
     );
 
-  const constructedUrl = new URL("/api/v1/user/get");
-  if (userId) constructedUrl.searchParams.set("user_id", userId);
+  let searchUrl = "/api/v1/user/get";
+  // Add query when using with an id
+  if (userId)
+    searchUrl += "?" + new URLSearchParams({ user_id: userId }).toString();
 
-  return await axios.get<ServiceReturn>(constructedUrl.toString());
+  return await axios.get<ServiceReturn>(searchUrl);
 }
