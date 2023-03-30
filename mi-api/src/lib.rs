6573d61f39014bf6a0dfa6d3c6784459b1f5dde6
Bora Arslan <bora.rs@pm.me>
2023-03-31 01:56:00 +0300

    add FutureLogExt trait

--- mi-api/src/lib.rs
+++ mi-api/src/lib.rs
@@ -1,9 +1,14 @@
-use std::future::Future;
+use std::{
+    future::Future,
+    pin::Pin,
+    task::{ready, Context, Poll},
+};
 
 use axum::extract::FromRequestParts;
 use axum::http;
 use axum::http::request::Parts;
 use hyper::StatusCode;
+use pin_project_lite::pin_project;
 use result::{AppError, AppResult};
 use state::AuthUser;
 use tokio::time::Instant;
@@ -49,3 +54,39 @@ pub async fn call_and_log_elapsed<T, E>(func: impl Future<Output = Result<T, E>>
     tracing::Span::current().record("elapsed", format!("{:.3?}", elapsed));
     res
 }
+
+pin_project! {
+    pub struct LogElapsed<F> {
+        #[pin]
+        future: F,
+    }
+}
+
+pub trait FutureLogExt: Future {
+    fn log_elapsed(self) -> LogElapsed<Self>
+    where
+        Self: Sized,
+    {
+        LogElapsed {
+            future: self,
+        }
+    }
+}
+
+impl<F> Future for LogElapsed<F>
+where
+    F: Future,
+{
+    type Output = F::Output;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let this = self.project();
+        let time = Instant::now();
+        let output = ready!(this.future.poll(cx));
+        let elapsed = time.elapsed();
+        tracing::Span::current().record("elapsed", format!("{:.3?}", elapsed));
+        Poll::Ready(output)
+    }
+}
+
+impl<T: ?Sized> FutureLogExt for T where T: Future {}
