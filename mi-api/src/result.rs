798d0ae50eacae5979c82c04b2f5c3753dd4abc7
Bora Arslan <bora.rs@pm.me>
2023-05-18 23:43:53 +0300

    switch AppResult

--- mi-api/src/result.rs
+++ mi-api/src/result.rs
@@ -1,190 +1,98 @@
 use axum::extract::rejection::JsonRejection;
-use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum_macros::FromRequest;
-use mi_db::auth::AuthError;
-use mi_db::user_lock::LockError;
-use mi_db::{InfluenceError, UserError};
+use mi_core::AppErrorExt;
+use mi_db::{AuthError, InfluenceError, LockError, UserError};
 use mi_osu_api::OsuApiError;
 use serde::Serialize;
+use thiserror::Error;
+use tracing::error;
 use validator::ValidationErrors;
 
+use crate::api::leaderboard::LeaderboardError;
+use crate::state::DB_POOL;
+use crate::SessionError;
+
+/// Custom JSON extractor that returns `AppError` if deserialization failed.
+///
+/// The reason of this structs existence is simply because we want to customize
+/// the rejection handling for JSON deserialization errors.
 #[derive(Debug, FromRequest)]
 #[from_request(via(axum::Json), rejection(AppError))]
 pub struct Json<T>(pub T);
 
-#[derive(Debug)]
-pub struct AppError(Kind);
-
-pub type AppResult<T> = Result<T, AppError>;
-
-impl AppError {
-    pub fn cookie_error() -> Self {
-        AppError(Kind::Cookie)
-    }
-
-    pub fn session_expired() -> Self {
-        AppError(Kind::Auth {
-            msg: "Session token is expired".to_string(),
-        })
-    }
-
-    pub fn osu_auth_error(msg: &str) -> Self {
-        AppError(Kind::Auth {
-            msg: format!("Osu! auth failed: {}", msg),
-        })
+impl<T: Serialize> IntoResponse for Json<T> {
+    fn into_response(self) -> axum::response::Response {
+        axum::Json(self.0).into_response()
     }
 }
 
-#[derive(Debug)]
-pub enum Kind {
-    OsuApi(OsuApiError),
-    Auth { msg: String },
-    User(UserError),
-    Influence(InfluenceError),
-    Validation { msg: String },
-    Cookie,
-    Json { code: StatusCode, msg: String },
-    Database { msg: String },
+/// Result returned by the application.
+pub type AppResult<T> = Result<T, AppError>;
+
+impl<T> From<AppError> for AppResult<T> {
+    fn from(value: AppError) -> Self {
+        Err(value)
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum AppError {
+    #[error(transparent)]
+    SessionError(#[from] SessionError),
+    #[error(transparent)]
+    LeaderboardError(#[from] LeaderboardError),
+    #[error(transparent)]
+    AuthError(#[from] AuthError),
+    #[error(transparent)]
+    OsuApiError(#[from] OsuApiError),
+    #[error(transparent)]
+    UserError(#[from] UserError),
+    #[error(transparent)]
+    InfluenceError(#[from] InfluenceError),
+    #[error(transparent)]
+    Validation(#[from] ValidationErrors),
+    #[error(transparent)]
+    LockError(#[from] LockError),
+    #[error(transparent)]
+    JsonRejection(#[from] JsonRejection),
+}
+
+impl From<AppError> for Box<dyn AppErrorExt> {
+    fn from(value: AppError) -> Self {
+        match value {
+            AppError::SessionError(e) => Box::new(e),
+            AppError::LeaderboardError(e) => Box::new(e),
+            AppError::AuthError(e) => Box::new(e),
+            AppError::OsuApiError(e) => Box::new(e),
+            AppError::UserError(e) => Box::new(e),
+            AppError::InfluenceError(e) => Box::new(e),
+            AppError::Validation(e) => Box::new(e),
+            AppError::LockError(e) => Box::new(e),
+            AppError::JsonRejection(e) => Box::new(e),
+        }
+    }
 }
 
 impl IntoResponse for AppError {
     fn into_response(self) -> axum::response::Response {
-        match self.0 {
-            Kind::OsuApi(osu_api_error) => {
-                let (code, msg) = match osu_api_error {
-                    OsuApiError::HTTPError { body, error } => (
-                        StatusCode::SERVICE_UNAVAILABLE,
-                        format!(
-                            "Response returned with error code {} and body: {}",
-                            error, body
-                        ),
-                    ),
-                    OsuApiError::InternalError(_) | OsuApiError::InvalidBeatmapType => {
-                        (StatusCode::INTERNAL_SERVER_ERROR, osu_api_error.to_string())
-                    }
-                };
+        let box_err: Box<dyn AppErrorExt> = self.into();
 
-                (
-                    code,
-                    format!("osu! API failed to make the HTTP request: {}", msg),
-                )
-                    .into_response()
-            }
-
-            Kind::Auth { msg } => {
-                (StatusCode::UNAUTHORIZED, format!("Auth is failed: {}", msg)).into_response()
-            }
-            Kind::User(user_error) => (
-                StatusCode::INTERNAL_SERVER_ERROR,
-                format!("Database error happened: {}", user_error),
-            )
-                .into_response(),
-            Kind::Cookie => {
-                (StatusCode::UNAUTHORIZED, "Token is invalid".to_string()).into_response()
-            }
-            Kind::Json { code, msg } => (code, msg).into_response(),
-            Kind::Influence(influence_err) => {
-                (StatusCode::INTERNAL_SERVER_ERROR, influence_err.to_string()).into_response()
-            }
-            Kind::Validation { msg } => (StatusCode::BAD_REQUEST, msg).into_response(),
-            Kind::Database { msg } => (StatusCode::INTERNAL_SERVER_ERROR, msg).into_response(),
+        if box_err.should_save() {
+            let err_db_obj = box_err.as_db_error_object();
+            tokio::spawn(async move {
+                let result = err_db_obj
+                    .insert_to_db(DB_POOL.get().expect(
+                        "Tried to get DB pool before initialization. This should never happen.",
+                    ))
+                    .await;
+                match result {
+                    Ok(_) => (),
+                    Err(e) => error!("Failed to save error to database: {}", e),
+                }
+            });
         }
-    }
-}
-
-impl From<OsuApiError> for AppError {
-    fn from(err: OsuApiError) -> Self {
-        AppError(Kind::OsuApi(err))
-    }
-}
-
-impl From<AuthError> for AppError {
-    fn from(err: AuthError) -> Self {
-        match err {
-            AuthError::ConnectionTimedOut => AppError(Kind::Auth {
-                msg: "Database connection timed out.".to_string(),
-            }),
-            AuthError::RedisError(_) => AppError(Kind::Auth {
-                msg: "Redis connection returned an error.".to_string(),
-            }),
-            AuthError::ValueNotFound => AppError(Kind::Auth {
-                msg: "Value is not found.".to_string(),
-            }),
-        }
-    }
-}
-
-impl From<LockError> for AppError {
-    fn from(err: LockError) -> Self {
-        match err {
-            LockError::ConnectionTimedOut => AppError(Kind::Auth {
-                msg: "Database connection timed out.".to_string(),
-            }),
-            LockError::RedisError(_) => AppError(Kind::Auth {
-                msg: "Redis connection returned an error.".to_string(),
-            }),
-        }
-    }
-}
-
-impl From<UserError> for AppError {
-    fn from(err: UserError) -> Self {
-        AppError(Kind::User(err))
-    }
-}
-
-impl From<InfluenceError> for AppError {
-    fn from(err: InfluenceError) -> Self {
-        AppError(Kind::Influence(err))
-    }
-}
-
-impl From<JsonRejection> for AppError {
-    fn from(rejection: JsonRejection) -> Self {
-        let code = match rejection {
-            JsonRejection::JsonDataError(_) => StatusCode::UNPROCESSABLE_ENTITY,
-            JsonRejection::JsonSyntaxError(_) => StatusCode::BAD_REQUEST,
-            JsonRejection::MissingJsonContentType(_) => StatusCode::UNSUPPORTED_MEDIA_TYPE,
-            _ => StatusCode::INTERNAL_SERVER_ERROR,
-        };
-        AppError(Kind::Json {
-            code,
-            msg: rejection.to_string(),
-        })
-    }
-}
-
-impl From<ValidationErrors> for AppError {
-    fn from(errors: ValidationErrors) -> Self {
-        let error = errors
-            .field_errors()
-            .into_values()
-            .next()
-            .expect("Errors can't be empty")
-            .first()
-            .expect("Errors can't be empty.");
-
-        AppError(Kind::Validation {
-            msg: error
-                .message
-                .as_ref()
-                .map(|cow| cow.to_string())
-                .unwrap_or("Unknown validation error.".to_string()),
-        })
-    }
-}
-
-impl From<sqlx::Error> for AppError {
-    fn from(error: sqlx::Error) -> Self {
-        AppError(Kind::Database {
-            msg: error.to_string(),
-        })
-    }
-}
-
-impl<T: Serialize> IntoResponse for Json<T> {
-    fn into_response(self) -> axum::response::Response {
-        axum::Json(self.0).into_response()
+        box_err.log_error();
+        box_err.as_response()
     }
 }
