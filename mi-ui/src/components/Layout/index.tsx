70d7f8bf6e65c3beebed1df6aacbc141bf1147ca
Fursum <fursum0@gmail.com>
2023-07-01 18:43:51 +0300

    add react-toastify

--- mi-ui/src/components/Layout/index.tsx
+++ mi-ui/src/components/Layout/index.tsx
@@ -2,10 +2,12 @@ import React, { FC, ReactNode } from "react";
 import Header from "./Header";
 import { useCurrentUser } from "@hooks/useUser";
 import Tooltip from "@components/SharedComponents/Tooltip";
+import { ToastContainer } from "react-toastify";
 
+import styles from "./style.module.scss";
 import "@fontsource-variable/inter";
 import "@fontsource-variable/comfortaa";
-import styles from "./style.module.scss";
+import "react-toastify/dist/ReactToastify.css";
 
 type Props = {
   children?: ReactNode;
@@ -19,6 +21,7 @@ const Layout: FC<Props> = ({ children }) => {
       {<Header />}
       <main className={styles.contentCenterer}>{children}</main>
       <Tooltip />
+      <ToastContainer />
     </>
   );
 };
